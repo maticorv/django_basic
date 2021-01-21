9cd84b4f444987aa11c1e21d5a16e7e4f8177158
Matias Corvalan <matias.corvalan24@gmail.com>
2021-01-20 22:48:54 -0300

    created

--- users/models.py
+++ users/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 class Profile(models.Model):
     """Profile Model."""
@@ -15,7 +16,7 @@ class Profile(models.Model):
         blank=True,
         null=True
     )
-    create = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):

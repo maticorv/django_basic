729198a35068936f2ffa0b4b8531ff52188fa07f
Matias Corvalan <matias.corvalan24@gmail.com>
2021-01-21 01:29:53 -0300

    modelo de post

--- posts/models.py
+++ posts/models.py
@@ -1,4 +1,20 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
 # Create your models here.
+from users.models import Profile
+
+
+class Post(models.Model):
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+    title = models.CharField(max_length=255)
+    photo = models.ImageField(upload_to='posts/photos')
+
+    created = models.DateTimeField(auto_now=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return '{} by @{}'.format(self.title, self.profile.user.username)

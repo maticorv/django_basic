51167dc8ca9551f1fad3b73824f33a67c42c018b
Matias Corvalan <matias.corvalan24@gmail.com>
2021-01-21 01:07:24 -0300

    admin page

--- users/admin.py
+++ users/admin.py
@@ -1,13 +1,15 @@
 # Django:
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 from django.contrib import admin
 
 # Models:
 from users.models import Profile
 
+
 # Register your models here.
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-
     # Variable con los campos a mostrar en el dashboard.
     list_display = (
         'pk',
@@ -46,4 +48,45 @@ class ProfileAdmin(admin.ModelAdmin):
         'user__is_active',
         'user__is_staff',
     )
+    fieldsets = (
+        ('Profile', {
+            'fields': (('user', 'picture'),)
+        }),
+        ('Extra info', {
+            'fields': (
+                ('website', 'phone_number'),
+                ('biography'),
+            )
+        }),
+        ('Metadata', {
+            'fields': (('created', 'modified'),),
+        })
+    )
+    readonly_fields = ('created', 'modified')
 
+
+"""
+Une los modelos de usuario y perfil para no tener que crear un usuario para asociarlo con un perfil
+"""
+
+
+class ProfileInLine(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'profiles'
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (ProfileInLine,)
+    list_display = (
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'is_active',
+        'is_staff'
+    )
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)

041225b357d8aa3a52f8861dabd2981f0d251178
Matias Corvalan <matias.corvalan24@gmail.com>
2021-02-16 02:12:18 -0300

    Formularios en django

--- users/views.py
+++ users/views.py
@@ -2,6 +2,8 @@ from django.contrib.auth import get_user_model, authenticate, login, logout
 from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
+from .forms import ProfileForm
+from django.contrib import messages
 
 
 # Create your views here.
@@ -39,4 +41,31 @@ def logout_view(request):
 
 
 def update_profile(request):
-    return render(request, 'users/update_profile.html')
+    profile = request.user.profile
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            data = form.cleaned_data
+
+            profile.website = data['website']
+            profile.phone_number = data['phone_number']
+            profile.biography = data['biography']
+            profile.picture = data['picture']
+            profile.save()
+            messages.success(request, 'Your profile has been updated!')
+
+            return redirect('update_profile')
+        else:
+            form = ProfileForm()
+
+    return render(
+        request,
+        'users/update_profile.html',
+        context={
+            'profile': profile,
+            'user': request.user,
+            # 'form': form
+        }
+    )
+
+
